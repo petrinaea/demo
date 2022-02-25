1f06d00e762a53593fbea8a2b6e9925b3d89652f
Ekaterina Petrina <kostyleva-katya@mail.ru>
2022-02-25 23:15:26 +0300

    added 4th task, created simple window, button "send" and input field

---
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+  @Override
+  public void start(Stage stage) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(HelloController.class.getResource("hello-view.fxml"));
+    Scene scene = new Scene(fxmlLoader.load());
+    stage.setTitle("Chat");
+    stage.setScene(scene);
+    stage.setY(500);
+    stage.setX(500);
+    stage.show();
+
+  }
+
+  public static void main(String[] args) {
+    launch();
+  }
+}
---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+public class HelloController {
+
+
+  @FXML
+  private ListView<String> resultMsg;
+
+  @FXML
+  private TextField inputField;
+
+
+  @FXML
+  void initialize() {
+  }
+
+
+  @FXML
+  public void sendMessage() {
+    String message = inputField.getText();
+    if (!message.isEmpty()){
+      resultMsg.getItems().add(message);
+    }
+    inputField.clear();
+
+  }
+
+
+}
